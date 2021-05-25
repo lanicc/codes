0ee874f2c38744c4ec804ecdce9b873bc17d40dd
lan <liaonan@souche.com>
2021-05-25 14:21:00 +0800

    update template

---
+++ codes-gen/template/api/param/DeleteParam.java
@@ -0,0 +1,31 @@
+package com.souche.component.${p.name}.api.model.param;
+
+import lombok.Data;
+
+import java.io.Serializable;
+<#list modelImports as mip>
+import ${mip.packageName}.${mip.name};
+</#list>
+
+/**
+ * ${model.desc}删除参数
+ *
+ * @author ${model.author}
+ * @since ${p.version}
+ */
+@Data
+public class ${model.name}DeleteParam implements Serializable {
+<#list model.fields as field>
+<#if field.primary>
+    /**
+     * ${field.desc}
+     */
+    ${field.scope} ${field.type} ${field.name};
+</#if>
+</#list>
+
+    /**
+     * 操作者
+     */
+    private String modifier;
+}
