0e320aeb5f9bb91831b76491df0bf6540b4091fb
黄佳明 <huangjiaming@souche.com>
2021-06-02 12:48:58 +0800

    feat:add ErrorInfo.java

---
+++ codes-gen/src/main/resources/template/api/ErrorInfo.java
@@ -0,0 +1,27 @@
+package com.souche.component.core.${p.name}.api.enums;
+
+import com.souche.component.sharing.common.api.IErrorInfo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Created on ${model.createTime}.
+ *
+ * @author ${model.author}
+ * @since ${p.version}
+ */
+@AllArgsConstructor
+public enum ${model.name}ErrorInfo implements IErrorInfo {
+
+        ${model.upperCaseName}_NOT_EXIST("5001", "${model.desc}不存在"),
+        ${model.upperCaseName}_INSERT_FAILED("5002", "${model.desc}新增失败"),
+        ${model.upperCaseName}_UPDATE_FAILED("5003", "${model.desc}更新失败");
+
+
+@Getter
+private final String errCode;
+
+@Getter
+private final String errMsg;
+
+}
